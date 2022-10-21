90b5d34cd5c58d558fa8bf474cfe796de1bcd749
nishant <nisupatel8200@gmail.com>
2022-10-21 16:17:32 +0530

    sucess

--- admin/index.php
+++ admin/index.php
@@ -3,17 +3,37 @@ require_once("includes/header.php");
 require_once("includes/navbar.php");
 
 ?>
-<div class="py-5  bg-dark">
-    <div class="continer">
+<style>
+ 
+</style>
+<div class="py-5">
+    <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <h3>Category</h3>
-                <div class="underline">
-
-                    
-                </div>
-                
+                <div class="underline"></div>
             </div>
+            <?php
+            $select = "select * from category ";
+            $result = mysqli_query($con, $select);
+            if (mysqli_num_rows($result) > 0) {
+                foreach ($result as $row) {
+            ?>
+                    <div class="col-md-3 mb-4">
+                        <a class="text-decoration-none" href="viewdata.php?title=<?php echo $row['slug']; ?>">
+                            <div class="card card-body">
+                                <?php echo $row['catergory_name']; ?>
+                            </div>
+                        </a>
+
+                    </div>
+
+            <?php
+                }
+            }
+
+            ?>
+
         </div>
     </div>
 </div>
